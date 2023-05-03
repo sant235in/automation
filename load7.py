bc87215059903517107a8c71a15d5763f914aa27
sant235in <sant235in@yahoo.co.in>
2023-05-03 17:01:22 -0600

    Update load7.py

--- load7.py
+++ load7.py
@@ -1,66 +1,79 @@
 import time
 import random
 import string
-import threading
-import couchdb
+import requests
 
-# Set up the database connection
-couch = couchdb.Server('http://localhost:5984')
-db = couch['mydatabase']
+COUCHDB_URL = 'http://localhost:5984'
+DB_NAME = 'test_db'
+NUM_THREADS = 10
+NUM_DOCS = 1000
 
-# Define the CRUD functions
-def insert_data():
-    for i in range(10000):
-        doc = {
-            'type': 'data',
-            'name': ''.join(random.choices(string.ascii_letters, k=10)),
-            'value': random.randint(1, 10000)
-        }
-        db.save(doc)
+def generate_random_string(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
 
-def update_data():
-    for i in range(10000):
-        doc_id = random.choice(list(db))
-        doc = db[doc_id]
-        doc['value'] = random.randint(1, 10000)
-        db.save(doc)
+def insert_document(doc_id):
+    doc = {'_id': doc_id, 'name': generate_random_string(10)}
+    res = requests.post(f'{COUCHDB_URL}/{DB_NAME}', json=doc)
+    return res.status_code == 201
 
-def delete_data():
-    for i in range(10000):
-        doc_id = random.choice(list(db))
-        db.delete(db[doc_id])
+def update_document(doc_id):
+    res = requests.get(f'{COUCHDB_URL}/{DB_NAME}/{doc_id}')
+    if res.status_code == 200:
+        doc = res.json()
+        doc['name'] = generate_random_string(10)
+        res = requests.put(f'{COUCHDB_URL}/{DB_NAME}/{doc_id}', json=doc)
+        return res.status_code == 201
+    else:
+        return False
 
-def read_data():
-    for i in range(10000):
-        doc_id = random.choice(list(db))
-        doc = db[doc_id]
+def read_document(doc_id):
+    res = requests.get(f'{COUCHDB_URL}/{DB_NAME}/{doc_id}')
+    return res.status_code == 200
 
-# Define the benchmark function
-def benchmark():
-    start_time = time.time()
+def delete_document(doc_id):
+    res = requests.get(f'{COUCHDB_URL}/{DB_NAME}/{doc_id}')
+    if res.status_code == 200:
+        doc = res.json()
+        res = requests.delete(f'{COUCHDB_URL}/{DB_NAME}/{doc_id}?rev={doc["_rev"]}')
+        return res.status_code == 200
+    else:
+        return False
 
-    # Create threads for each CRUD operation
-    threads = []
-    insert_thread = threading.Thread(target=insert_data)
-    update_thread = threading.Thread(target=update_data)
-    delete_thread = threading.Thread(target=delete_data)
-    read_thread = threading.Thread(target=read_data)
-    threads.append(insert_thread)
-    threads.append(update_thread)
-    threads.append(delete_thread)
-    threads.append(read_thread)
+def run_benchmark():
+    print('Starting benchmark...')
+    insert_times = []
+    update_times = []
+    read_times = []
+    delete_times = []
 
-    # Start the threads
-    for thread in threads:
-        thread.start()
+    doc_ids = [f'doc{i}' for i in range(NUM_DOCS)]
 
-    # Wait for the threads to finish
-    for thread in threads:
-        thread.join()
+    for i in range(NUM_THREADS):
+        insert_times.append([])
+        update_times.append([])
+        read_times.append([])
+        delete_times.append([])
 
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print(f"Elapsed time: {elapsed_time} seconds")
+    for doc_id in doc_ids:
+        start_time = time.time()
 
-# Run the benchmark function
-benchmark()
+        # Simultaneously perform CRUD operations for each document
+        for i in range(NUM_THREADS):
+            if insert_document(doc_id):
+                insert_times[i].append(time.time() - start_time)
+            if update_document(doc_id):
+                update_times[i].append(time.time() - start_time)
+            if read_document(doc_id):
+                read_times[i].append(time.time() - start_time)
+            if delete_document(doc_id):
+                delete_times[i].append(time.time() - start_time)
+
+    # Print the average time taken for each operation by each thread
+    print(f'Insert times: {[sum(insert_times[i])/len(insert_times[i]) for i in range(NUM_THREADS)]}')
+    print(f'Update times: {[sum(update_times[i])/len(update_times[i]) for i in range(NUM_THREADS)]}')
+    print(f'Read times: {[sum(read_times[i])/len(read_times[i]) for i in range(NUM_THREADS)]}')
+    print(f'Delete times: {[sum(delete_times[i])/len(delete_times[i]) for i in range(NUM_THREADS)]}')
+
+if __name__ == '__main__':
+    run_benchmark()
