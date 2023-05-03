e226ff0e0205ae847eec17a8947aa8315b44ca62
sant235in <sant235in@yahoo.co.in>
2023-05-03 16:43:10 -0600

    Update load7.py

--- load7.py
+++ load7.py
@@ -1,88 +1,69 @@
 import time
 import random
 import string
-import requests
+import couchdb
 
-COUCHDB_URL = "http://localhost:5984"
-DB_NAME = "test_db"
-NUM_OPERATIONS = 1000
+# Set up connection to CouchDB
+couch = couchdb.Server('http://localhost:5984/')
+db_name = 'test_db'
+if db_name not in couch:
+    db = couch.create(db_name)
+else:
+    db = couch[db_name]
 
-# Generate random data
-def generate_data():
-    data = {
-        "name": "".join(random.choices(string.ascii_letters, k=10)),
-        "age": random.randint(1, 100),
-        "address": "".join(random.choices(string.ascii_letters + string.digits, k=20))
-    }
-    return data
+# Define functions for CRUD operations
+def create_document():
+    doc_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+    doc_data = {'random_data': ''.join(random.choices(string.ascii_uppercase + string.digits, k=100))}
+    db.save(doc_data)
+    return doc_id
 
-# Measure the time taken to perform a request
-def measure_time(func):
-    start_time = time.time()
-    func()
-    end_time = time.time()
-    return end_time - start_time
+def read_document(doc_id):
+    doc = db.get(doc_id)
+    return doc['_rev']
 
-# Insert random data into the database
-def insert_data():
-    data = generate_data()
-    response = requests.post(f"{COUCHDB_URL}/{DB_NAME}", json=data)
-    response.raise_for_status()
+def update_document(doc_id, doc_rev):
+    doc = db.get(doc_id)
+    doc['random_data'] = ''.join(random.choices(string.ascii_uppercase + string.digits, k=100))
+    db.save(doc)
 
-# Update random data in the database
-def update_data():
-    response = requests.get(f"{COUCHDB_URL}/{DB_NAME}/_all_docs")
-    response.raise_for_status()
-    doc_ids = [row["id"] for row in response.json()["rows"]]
-    if len(doc_ids) > 0:
-        doc_id = random.choice(doc_ids)
-        response = requests.get(f"{COUCHDB_URL}/{DB_NAME}/{doc_id}")
-        response.raise_for_status()
-        data = response.json()
-        data["name"] = "".join(random.choices(string.ascii_letters, k=10))
-        response = requests.put(f"{COUCHDB_URL}/{DB_NAME}/{doc_id}", json=data)
-        response.raise_for_status()
+def delete_document(doc_id, doc_rev):
+    doc = db.get(doc_id)
+    db.delete(doc)
 
-# Delete random data from the database
-def delete_data():
-    response = requests.get(f"{COUCHDB_URL}/{DB_NAME}/_all_docs")
-    response.raise_for_status()
-    doc_ids = [row["id"] for row in response.json()["rows"]]
-    if len(doc_ids) > 0:
-        doc_id = random.choice(doc_ids)
-        response = requests.get(f"{COUCHDB_URL}/{DB_NAME}/{doc_id}")
-        response.raise_for_status()
-        data = response.json()
-        response = requests.delete(f"{COUCHDB_URL}/{DB_NAME}/{doc_id}?rev={data['_rev']}")
-        response.raise_for_status()
+# Define function to capture performance benchmark of CRUD operations
+def measure_performance():
+    create_times = []
+    read_times = []
+    update_times = []
+    delete_times = []
 
-# Read random data from the database
-def read_data():
-    response = requests.get(f"{COUCHDB_URL}/{DB_NAME}/_all_docs")
-    response.raise_for_status()
-    doc_ids = [row["id"] for row in response.json()["rows"]]
-    if len(doc_ids) > 0:
-        doc_id = random.choice(doc_ids)
-        response = requests.get(f"{COUCHDB_URL}/{DB_NAME}/{doc_id}")
-        response.raise_for_status()
+    for i in range(1000):
+        # Measure time for create operation
+        start_time = time.time()
+        doc_id = create_document()
+        create_times.append(time.time() - start_time)
 
-# Measure the performance of each operation
-insert_times = []
-update_times = []
-delete_times = []
-read_times = []
-for i in range(NUM_OPERATIONS):
-    insert_times.append(measure_time(insert_data))
-    update_times.append(measure_time(update_data))
-    delete_times.append(measure_time(delete_data))
-    read_times.append(measure_time(read_data))
+        # Measure time for read operation
+        start_time = time.time()
+        doc_rev = read_document(doc_id)
+        read_times.append(time.time() - start_time)
 
-# Output the results
-print("Insert times (s):")
-print(f"    min: {min(insert_times)}")
-print(f"    max: {max(insert_times)}")
-print(f"    avg: {sum(insert_times) / len(insert_times)}")
-print("Update times (s):")
-print(f"    min: {min(update_times)}")
-print(f"    max: {max(update_times)}")
-print(f
+        # Measure time for update operation
+        start_time = time.time()
+        update_document(doc_id, doc_rev)
+        update_times.append(time.time() - start_time)
+
+        # Measure time for delete operation
+        start_time = time.time()
+        delete_document(doc_id, doc_rev)
+        delete_times.append(time.time() - start_time)
+
+    # Print performance benchmark results
+    print(f'Average create time: {sum(create_times)/len(create_times)} seconds')
+    print(f'Average read time: {sum(read_times)/len(read_times)} seconds')
+    print(f'Average update time: {sum(update_times)/len(update_times)} seconds')
+    print(f'Average delete time: {sum(delete_times)/len(delete_times)} seconds')
+
+if __name__ == '__main__':
+    measure_performance()
