2dc1efa9f29a07c049ee5e6ac581942567c93869
sant235in <sant235in@yahoo.co.in>
2023-05-06 12:10:14 -0600

    Create mongo_users1.sh

---
+++ mongo_users1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Set the MongoDB connection parameters
+HOST="localhost"
+PORT="27017"
+USERNAME="admin"
+PASSWORD="password"
+
+# Get the list of all databases
+DATABASES=$(mongo --host $HOST:$PORT --authenticationDatabase admin --username $USERNAME --password $PASSWORD --quiet --eval "db.adminCommand({listDatabases: 1})" | jq -r '.databases[].name')
+
+# Iterate over each database and list all users and their roles
+for db in $DATABASES
+do
+  # Skip the system databases
+  if [[ $db == "admin" || $db == "local" || $db == "config" ]]; then
+    continue
+  fi
+
+  # Get the list of all users and their roles
+  USERS=$(mongo --host $HOST:$PORT --authenticationDatabase admin --username $USERNAME --password $PASSWORD --quiet $db --eval "db.runCommand({usersInfo: 1})" | jq -r '.users[] | "\(.user),\(.roles | map(select(.role != "") | .role) | join(","))"')
+
+  # Print the database name and the list of users and their roles
+  echo "Database: $db"
+  echo "$USERS"
+  echo ""
+done
