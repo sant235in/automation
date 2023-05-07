abe12569846226342bd79ed4542ad178ac01cc85
sant235in <sant235in@yahoo.co.in>
2023-05-07 01:15:24 -0600

    Update ha2.py

--- ha2.py
+++ ha2.py
@@ -1,77 +1,70 @@
+import requests
+import json
 import time
-import random
-import couchdb
-from uuid import uuid4
 
-# Connection parameters for the CouchDB cluster
-server_url = 'http://localhost:5984/'
-db_name = 'mydatabase'
-username = 'admin'
-password = 'password'
-
-# Number of documents to be inserted
+# Configurations
+base_url = "http://<haproxy-node>:<port>"
+db_name = "test_db"
+username = "<couchdb-username>"
+password = "<couchdb-password>"
+doc_size = 1024  # in bytes
 num_docs = 1000
+num_iterations = 10
 
-# Size of each document in KB
-doc_size = 1
-
-# Field to create index on
-index_field = 'field'
-
-# Connect to the CouchDB cluster
-couch = couchdb.Server(server_url)
-couch.resource.credentials = (username, password)
-
-# Create a new database
-try:
-    db = couch.create(db_name)
-except couchdb.http.PreconditionFailed as e:
-    db = couch[db_name]
-
-# Enable sharding on the database
-db.enable_sharding()
-
-# Create the index on the specified field
-db.create_index([index_field])
-
-# Generate random data with a specified document size
-def generate_data(size):
-    data = {}
-    for i in range(size):
-        data[str(i)] = str(uuid4())
-    return data
-
-# Insert a document into the database with write acknowledgment w=2
-def insert_doc(db, data):
-    doc_id, doc_rev = db.save(data, w=2)
-    return doc_id, doc_rev
-
-# Update a document in the database with write acknowledgment w=2
-def update_doc(db, doc_id, data):
-    doc = db[doc_id]
-    doc.update(data)
-    doc_id, doc_rev = db.save(doc, w=2)
-    return doc_id, doc_rev
-
-# Read a document from the database with read acknowledgment r=2
-def read_doc(db, doc_id):
-    doc = db.get(doc_id, r=2)
-    return doc
-
-# Insert, update, and read random data from the database simultaneously
-start_time = time.time()
+# Generate random data with index on a field
+docs = []
 for i in range(num_docs):
-    data = generate_data(doc_size)
-    doc_id, doc_rev = insert_doc(db, data)
-    data[index_field] = random.randint(0, doc_size-1)
-    doc_id, doc_rev = update_doc(db, doc_id, data)
-    doc = read_doc(db, doc_id)
-end_time = time.time()
+    doc = {"index": i}
+    doc["data"] = "a" * doc_size
+    docs.append(doc)
 
-# Calculate the total time taken and the throughput
-total_time = end_time - start_time
-throughput = num_docs / total_time
+# Set up authentication header
+auth_header = requests.auth.HTTPBasicAuth(username, password)
 
-# Print the results
-print("Total time taken: {:.2f} seconds".format(total_time))
-print("Throughput: {:.2f} docs/sec".format(throughput))
+# Create database if it does not exist
+db_url = f"{base_url}/{db_name}"
+db_exists = requests.head(db_url, auth=auth_header)
+if db_exists.status_code == 404:
+    requests.put(db_url, auth=auth_header)
+
+# Start benchmarking
+total_time = 0
+for i in range(num_iterations):
+    print(f"Iteration {i+1}")
+    start_time = time.time()
+
+    # Insert documents
+    for doc in docs:
+        url = f"{db_url}/{doc['index']}"
+        headers = {"Content-Type": "application/json"}
+        response = requests.put(url, data=json.dumps(doc), headers=headers, auth=auth_header, params={"w": 2})
+        assert response.ok
+
+    # Read documents
+    for doc in docs:
+        url = f"{db_url}/{doc['index']}"
+        response = requests.get(url, auth=auth_header, params={"r": 2})
+        assert response.ok
+
+    # Update documents
+    for doc in docs:
+        url = f"{db_url}/{doc['index']}"
+        doc["data"] = "b" * doc_size
+        headers = {"Content-Type": "application/json"}
+        response = requests.put(url, data=json.dumps(doc), headers=headers, auth=auth_header, params={"w": 2})
+        assert response.ok
+
+    # Delete documents
+    for doc in docs:
+        url = f"{db_url}/{doc['index']}"
+        response = requests.delete(url, auth=auth_header, params={"w": 2})
+        assert response.ok
+
+    end_time = time.time()
+    iteration_time = end_time - start_time
+    total_time += iteration_time
+    print(f"Iteration time: {iteration_time:.2f}s")
+
+# Calculate average time per iteration
+avg_time = total_time / num_iterations
+print(f"Average time per iteration: {avg_time:.2f}s")
