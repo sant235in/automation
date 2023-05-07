1131ab0adc31ec84c4ec5b54a7ec5ef8783960fb
sant235in <sant235in@yahoo.co.in>
2023-05-06 21:11:04 -0600

    Update mongo_users4.py

--- mongo_users4.py
+++ mongo_users4.py
@@ -1,35 +1,28 @@
 import pymongo
 import ssl
 
-# Set the connection parameters
+# Set the MongoDB connection parameters
 HOST = "localhost"
 PORT = 27017
 USERNAME = "admin"
 PASSWORD = "password"
-CAFILE = "/path/to/cafile.crt"
-KEYFILE = "/path/to/keyfile.pem"
-KEYFILE_PASSWORD = "keyfile_password"
+TLS_CA_FILE = "/path/to/ca.crt"
+TLS_KEY_FILE = "/path/to/client.pem"
+TLS_KEY_PASSWORD = "key_password"
 
-# Connect to the MongoDB instance with TLS and a keyfile with password
-ssl_context = ssl.create_default_context(cafile=CAFILE)
-ssl_context.load_cert_chain(KEYFILE, password=KEYFILE_PASSWORD)
-client = pymongo.MongoClient(host=HOST, port=PORT, ssl=True, ssl_context=ssl_context)
-db = client.admin
-db.authenticate(USERNAME, PASSWORD)
+# Create a MongoDB client with TLS and authentication options
+tls_context = ssl.create_default_context(cafile=TLS_CA_FILE)
+tls_context.load_cert_chain(TLS_KEY_FILE, password=TLS_KEY_PASSWORD)
+client = pymongo.MongoClient(host=HOST, port=PORT, tls=True, tlsCAFile=TLS_CA_FILE, tlsCertificateKeyFile=TLS_KEY_FILE, username=USERNAME, password=PASSWORD, authSource="admin", authMechanism="SCRAM-SHA-256", ssl_context=tls_context)
 
-# Get a list of all the non-system databases
-databases = client.list_database_names()
-databases = [db_name for db_name in databases if db_name not in ['admin', 'config', 'local']]
+# Get the list of non-system databases
+db_names = [db for db in client.list_database_names() if db not in ["admin", "config", "local"]]
 
-# Loop through each database and list the users and their roles
-for database in databases:
-    # Switch to the current database
-    db = client[database]
-    
-    # Get a list of all the users and their roles in the current database
-    users = db.command({'usersInfo': 1})['users']
-    
-    # Loop through each user and print the database name, user name, and role name in CSV format
+# Iterate over each database and list its users and roles
+for db_name in db_names:
+    db = client[db_name]
+    users = db.command("usersInfo")["users"]
     for user in users:
-        roles = [role['role'] for role in user['roles']]
-        print(f"{database},{user['user']},{'|'.join(roles)}")
+        username = user["user"]
+        roles = user["roles"]
+        print(f"{db_name}, {username}, {','.join(roles)}")
