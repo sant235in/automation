e8e37e977548b8b0e5bec272036be6090662eb2f
sant235in <sant235in@yahoo.co.in>
2023-05-06 21:16:30 -0600

    Update mongo_users4.py

--- mongo_users4.py
+++ mongo_users4.py
@@ -15,14 +15,17 @@ tls_context = ssl.create_default_context(cafile=TLS_CA_FILE)
 tls_context.load_cert_chain(TLS_KEY_FILE, password=TLS_KEY_PASSWORD)
 client = pymongo.MongoClient(host=HOST, port=PORT, tls=True, tlsCAFile=TLS_CA_FILE, tlsCertificateKeyFile=TLS_KEY_FILE, username=USERNAME, password=PASSWORD, authSource="admin", authMechanism="SCRAM-SHA-256", ssl_context=tls_context)
 
-# Get the list of non-system databases
-db_names = [db for db in client.list_database_names() if db not in ["admin", "config", "local"]]
+# Get a list of all the databases in the instance
+databases = client.list_database_names()
 
 # Iterate over each database and list its users and roles
-for db_name in db_names:
-    db = client[db_name]
-    users = db.command("usersInfo")["users"]
-    for user in users:
-        username = user["user"]
-        roles = user["roles"]
-        print(f"{db_name}, {username}, {','.join(roles)}")
+for db_name in databases:
+    # Skip the system databases
+    if db_name not in ["admin", "config", "local"]:
+        # Select the database and get its users
+        db = client[db_name]
+        users = db.command("usersInfo")
+        # Iterate over each user and print its name and roles
+        for user in users["users"]:
+            roles = ",".join([role["role"] for role in user["roles"]])
+            print(f"{db_name},{user['user']},{roles}")
