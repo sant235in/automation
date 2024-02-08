14530b01c06cd9af1415407c9bb44bfcec66b47d
sant235in <sant235in@yahoo.co.in>
2024-02-08 10:18:04 -0700

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,41 +1,6 @@
-import couchdb
-import random
-import string
-
-# Change these variables according to your CouchDB configuration
-COUCHDB_URL = 'http://localhost:5984'
-DB_NAME = 'your_database_name'
-
-# Function to generate random string for the document content
-def generate_random_string(length):
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for _ in range(length))
-
-# Function to insert documents into the database
-def insert_documents(db, num_documents):
-    for i in range(num_documents):
-        # Change 'partition_key' to the desired partition key field name
-        partition_key = f'partition-{i % 10}'  # 10 partitions in this example
-        content = generate_random_string(10)
-        doc = {
-            '_id': f'doc{i}',
-            'partition_key': partition_key,
-            'content': content,
-        }
-        db.save(doc)
-
-# Main function
-def main():
-    couch = couchdb.Server(COUCHDB_URL)
-    try:
-        db = couch.create(DB_NAME)
-    except couchdb.http.PreconditionFailed:
-        db = couch[DB_NAME]
-
-    num_documents = 100  # Change this to the desired number of documents
-    insert_documents(db, num_documents)
-
-    print(f'{num_documents} documents inserted into the database.')
-
-if __name__ == '__main__':
-    main()
+import groovy.json.JsonSlurperimport java.util.Arrays
+// def arrayList = ['dev', 'qa', 'qdeva', 'uat']
+// println list
+def return_list = []def file = "/Users/A200167708/.jenkins/workspace/json-check-pipeline/file.json"def jsonString = new File(file).textdef environmentSpecific = new JsonSlurper().parseText(jsonString)['EnvironmentSpecific']
+environmentSpecific.each { env, configurations -> // println "Adding choice: $env" return_list.add("\'$env\'")}
+// println "Choices: $return_list"println(return_list.getClass())println return_listdef newlist = Arrays.asList(return_list.toArray())println(newlist.getClass())println newlistreturn return_list
